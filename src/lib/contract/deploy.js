cd38801a14dca88bab381442b100fcad32e65eb9
Doug <douganderson444@gmail.com>
2021-09-16 14:36:44 -0300

    chg: bundle details into one object

--- src/lib/contract/deploy.js
+++ src/lib/contract/deploy.js
@@ -4,17 +4,14 @@ import { createContract } from 'smartweave';
 import * as src from './append.js';
 import state from './initial-state.json'; // rollup-plugin-string lets us do this in dev mode
 
-export const deploy = async ({
-	client,
-	payer,
-	owner,
-	latest = { ipfs: 'IPFS CID', arweave: 'Arweave Tx Id' } // optional init balance, else random owner amt
-}) => {
-	state.name = name;
-	state.owner = owner;
-	state.latest = latest; // { ipfs, arweave }
+export const deploy = async ({ client, payer, details }) => {
+	if (!details.owner) throw new Error('Contract must be owned');
+	state.name = details.name || 'unnamed';
+	state.owner = details.owner;
+	state.latest = details.latest || { ipfs: 'IPFS CID', arweave: 'Arweave Tx Id' };
 	const source = src.default;
 	console.log({ client, payer, source, state });
 	const id = await createContract(client, payer, source, JSON.stringify(state));
+	console.log({ id });
 	return id;
 };
